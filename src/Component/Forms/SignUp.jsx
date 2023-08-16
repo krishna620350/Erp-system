eadbc02eefaa48299fc729078cb028174647fa64
krishna620350 <manishkumar620350@gmail.com>
2023-08-16 19:29:17 +0530

    first commit

--- src/Component/Forms/SignUp.jsx
+++ src/Component/Forms/SignUp.jsx
@@ -1,3 +1,4 @@
+import { useState} from "react";
 import { Link } from "react-router-dom";
 import data from "../../Json/State.json"
 import { schoolRegister } from "../../Api/schoolRegistrationApi";
